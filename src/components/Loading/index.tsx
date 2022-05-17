e3a1db0cf8ba189c03565bcbbbcef428dfc3e642
JeanVictorMachado <jeanvictor.arq@gmail.com>
2022-05-17 13:28:07 -0300

    feat: updated

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,17 +1,30 @@
 import { LogoApple } from "../LogoApple";
+
 import "./styles.css";
 
-export const Loading = () => {
+interface LoadingProps {
+  isLogo?: boolean;
+}
+
+export const Loading = ({ isLogo = true }: LoadingProps) => {
   return (
     <div className="loading__container">
-      <div className="wrapper">
-        <div className="logo">
-          <LogoApple />
+      <div
+        className="wrapper"
+        style={{
+          top: `${isLogo ? "60%" : "50%"}`,
+        }}
+      >
+        {isLogo && (
+          <div className="logo">
+            <LogoApple />
 
-          <p className="logo-text">
-            Keto <p className="logo-text-pro">PRO</p>
-          </p>
-        </div>
+            <p className="logo-text">
+              Keto <p className="logo-text-pro">PRO</p>
+            </p>
+          </div>
+        )}
+
         <div className="circle" />
         <div className="circle" />
         <div className="circle" />

eb71e088367afe1443046d84e51177c7ab032d2b
JeanVictorMachado <jeanvictor.arq@gmail.com>
2022-05-17 13:14:04 -0300

    feat: creating logo loading

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,9 +1,17 @@
+import { LogoApple } from "../LogoApple";
 import "./styles.css";
 
 export const Loading = () => {
   return (
     <div className="loading__container">
       <div className="wrapper">
+        <div className="logo">
+          <LogoApple />
+
+          <p className="logo-text">
+            Keto <p className="logo-text-pro">PRO</p>
+          </p>
+        </div>
         <div className="circle" />
         <div className="circle" />
         <div className="circle" />

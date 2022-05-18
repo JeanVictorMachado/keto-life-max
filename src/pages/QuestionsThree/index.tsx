788eaaf956c9f9f39cdb342adfa6fe6f52704318
consultor.jean <consultor.jean@leomadeiras.com.br>
2022-05-18 17:29:08 -0300

    feat: updated

--- src/pages/QuestionsThree/index.tsx
+++ src/pages/QuestionsThree/index.tsx
@@ -6,9 +6,16 @@ import { GreenCircle } from "../../components/GreenCircle";
 import { Header } from "../../components/Header";
 import { InputCalculation } from "../../components/InputCalculation";
 import { SubmitButton } from "../../components/SubmitButton";
+import { validateInputs } from "../../helpers/validateInputs";
 
 import "./styles.css";
-import { validateInputs } from "../../helpers/validateInputs";
+
+interface ValidateErrorsProps {
+  isAgeError: string;
+  isHeightError: string;
+  isWeightError: string;
+  isDesiredWeightError: string;
+}
 
 interface InputValuesProps {
   age: string;
@@ -32,6 +39,58 @@ export const QuestionsThree = () => {
   const [weightValue, setWeightValue] = useState("");
   const [desiredWeightValue, setDesiredWeightValue] = useState("");
 
+  const validateErrors = ({
+    isAgeError,
+    isHeightError,
+    isWeightError,
+    isDesiredWeightError,
+  }: ValidateErrorsProps) => {
+    isAgeError.length > 0 && setAgeValue("");
+    isHeightError.length > 0 && setHeightValue("");
+    isWeightError.length > 0 && setWeightValue("");
+    isDesiredWeightError.length > 0 && setDesiredWeightValue("");
+
+    if (
+      isAgeError.length === 0 &&
+      isHeightError.length === 0 &&
+      isWeightError.length === 0 &&
+      isDesiredWeightError.length === 0
+    ) {
+      navigate("/");
+    }
+  };
+
+  const handleErrorAndRedirect = () => {
+    const isAgeError = validateInputs({
+      inputName: "age",
+      value: inputValues.age,
+    });
+    const isHeightError = validateInputs({
+      inputName: "height",
+      value: inputValues.height,
+    });
+    const isWeightError = validateInputs({
+      inputName: "weight",
+      value: inputValues.weight,
+    });
+    const isDesiredWeightError = validateInputs({
+      inputName: "desiredWeight",
+      value: inputValues.desiredWeight,
+    });
+
+    setAgeError(isAgeError);
+    setHeightError(isHeightError);
+    setWeightError(isWeightError);
+    setDesiredWeightError(isDesiredWeightError);
+
+    validateErrors({
+      isAgeError,
+      isHeightError,
+      isWeightError,
+      isDesiredWeightError,
+    });
+  };
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const name = event.target.name;
     const value = event.target.value;
@@ -54,30 +113,7 @@ export const QuestionsThree = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-    setAgeError(
-      validateInputs({
-        inputName: "age",
-        value: inputValues.age,
-      })
-    );
-    setHeightError(
-      validateInputs({
-        inputName: "height",
-        value: inputValues.height,
-      })
-    );
-    setWeightError(
-      validateInputs({
-        inputName: "weight",
-        value: inputValues.weight,
-      })
-    );
-    setDesiredWeightError(
-      validateInputs({
-        inputName: "desiredWeight",
-        value: inputValues.desiredWeight,
-      })
-    );
+    handleErrorAndRedirect();
   };
 
   return (

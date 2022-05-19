fc84d8a6f5f864142000e44669f9ceea71b95fd8
consultor.jean <consultor.jean@leomadeiras.com.br>
2022-05-19 12:06:10 -0300

    feat: updated

--- src/pages/Results/index.tsx
+++ src/pages/Results/index.tsx
@@ -1,11 +1,19 @@
+import { useMemo } from "react";
 import { Footer } from "../../components/Footer";
 import { GreenCircleResult } from "../../components/GreenCircleResult";
 import { Loading } from "../../components/Loading";
 import { LogoKetoPro } from "../../components/LogoKetoPro";
 
+import { MdOutlineFaceRetouchingNatural } from "react-icons/md";
+import { BiFace } from "react-icons/bi";
+
 import "./styles.css";
 
 export const Results = () => {
+  const gender = useMemo(() => {
+    return localStorage.getItem("@ketopro__gender:");
+  }, []);
+
   return (
     <>
       {false ? (
@@ -17,7 +25,15 @@ export const Results = () => {
           </div>
 
           <div className="results__green-circle-container">
-            <GreenCircleResult />
+            <GreenCircleResult
+              svg={
+                gender === "male" ? (
+                  <BiFace size={60} color="#606060" />
+                ) : (
+                  <MdOutlineFaceRetouchingNatural size={60} color="#606060" />
+                )
+              }
+            />
           </div>
 
           <div className="results__content"></div>

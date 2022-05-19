4091dfdf3baf336e25df7e9f6ca7cb1fafd67171
consultor.jean <consultor.jean@leomadeiras.com.br>
2022-05-19 11:53:00 -0300

    feat: updated

--- src/components/GreenCircleResult/index.tsx
+++ src/components/GreenCircleResult/index.tsx
@@ -6,11 +6,11 @@ interface GreenCircleProps {
 
 export const GreenCircleResult = ({ svg: Svg }: GreenCircleProps) => {
   return (
-    <div className="green-circle__container-green-circle">
-      <div className="green-circle__container-green-circle">
-        <div className="green-circle__green-circle-3" />
-        <div className="green-circle__green-circle-2" />
-        <div className="green-circle__green-circle-1">{Svg}</div>
+    <div className="green-circle-results__container-green-circle">
+      <div className="green-circle-results__container-green-circle">
+        <div className="green-circle-results__green-circle-3" />
+        <div className="green-circle-results__green-circle-2" />
+        <div className="green-circle-results__green-circle-1">{Svg}</div>
       </div>
     </div>
   );

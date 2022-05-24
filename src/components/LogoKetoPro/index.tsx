525466945b6332588375d0d5af5e3421aa555e4f
consultor.jean <consultor.jean@leomadeiras.com.br>
2022-05-24 17:42:53 -0300

    feat: updated

--- src/components/LogoKetoPro/index.tsx
+++ src/components/LogoKetoPro/index.tsx
@@ -3,19 +3,20 @@ import "./styles.css";
 export const LogoKetoPro = () => {
   return (
     <div className="header__container-logo">
+      <div>
+        <p className="header-title">
+          Keto <p className="header-title-life">Life</p>
+          <p className="header-title-max">MAX</p>
+        </p>
+
+        {/* <span className="header-logo-sub-title">EMAGREÇA COM SAÚDE</span> */}
+      </div>
+
       <img
         className="header__logo"
         src="images/logo-keto-pro.svg"
         alt="Logotipo Keto Life"
       />
-
-      <div>
-        <p className="header-title">
-          Keto <p className="header-title-pro">Life</p>
-        </p>
-
-        <span className="header-logo-sub-title">EMAGREÇA COM SAÚDE</span>
-      </div>
     </div>
   );
 };

da09ec846115a9eb0bf0bcb24466d122b286574e
consultor.jean <consultor.jean@leomadeiras.com.br>
2022-05-26 10:19:51 -0300

    feat: updated

--- src/components/LogoKetoPro/index.tsx
+++ src/components/LogoKetoPro/index.tsx
@@ -3,14 +3,10 @@ import "./styles.css";
 export const LogoKetoPro = () => {
   return (
     <div className="header__container-logo">
-      <div>
-        <p className="header-title">
-          Keto <p className="header-title-life">Life</p>
-          <p className="header-title-max">MAX</p>
-        </p>
-
-        {/* <span className="header-logo-sub-title">EMAGREÇA COM SAÚDE</span> */}
-      </div>
+      <p className="header-title">
+        Keto <p className="header-title-life">Life</p>
+        <p className="header-title-max">MAX</p>
+      </p>
 
       <img
         className="header__logo"

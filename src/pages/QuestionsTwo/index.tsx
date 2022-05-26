dfaacbff1335392336a999d1c52424a9fa1f2111
consultor.jean <consultor.jean@leomadeiras.com.br>
2022-05-26 10:47:42 -0300

    feat: updated

--- src/pages/QuestionsTwo/index.tsx
+++ src/pages/QuestionsTwo/index.tsx
@@ -43,7 +43,7 @@ export const QuestionsTwo = () => {
       }}
     >
       <Header
-        image="./images/life-style-family.png"
+        image="./images/mudanca-habitos.jpg"
         icon={<GreenCircle svg={<FaRunning fontSize={30} />} />}
       />
 
